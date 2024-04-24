86ce402145d6700f45d853a2541f11ce1cfa0273
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-04-24 22:36:06 +0000

    order domain

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/domain/Order.java
@@ -0,0 +1,77 @@
+package ch.fhnw.pizza.data.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    
+    
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "order")
+    private List<Pizza> pizzas = new ArrayList<>();
+    
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Column(name = "finalprice")
+    private Double finalprice;
+
+    public Double getFinalprice() {
+        return finalprice;
+    }
+
+    public void setFinalprice(Double finalprice) {
+        this.finalprice = finalprice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public void setPizzas(List<Pizza> pizzas) {
+        this.pizzas = pizzas;
+    }
+    
+
+    
+
+
+
+}
