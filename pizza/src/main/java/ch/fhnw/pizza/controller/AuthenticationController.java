b13b5744754cc2a1f33e055c2fc2362be238b0a1
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-01 23:53:24 +0000

    required changes

--- pizza/src/main/java/ch/fhnw/pizza/controller/AuthenticationController.java
+++ pizza/src/main/java/ch/fhnw/pizza/controller/AuthenticationController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 
 
@@ -19,6 +18,7 @@ public class AuthenticationController {
     @PostMapping("/login") // changed from Getmapping 
     //public String getLogin(@RequestParam String username, @RequestParam String password) {
         //if (username.equals("admin") && password.equals("password")) {
+       //public String getLogin(@RequestBody User user) {     
         public RedirectView getLogin(@RequestBody User user) {    
         if (user.getUserName().equals("admin") && user.getPassword().equals("password")) {    
            // return "Login successful!";
