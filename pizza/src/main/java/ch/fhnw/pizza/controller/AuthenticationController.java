f9ee736430b13f63b3f5e8bab32ad020d9e905d4
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-01 23:41:48 +0000

    changes to redirection to userprofile when logged in

--- pizza/src/main/java/ch/fhnw/pizza/controller/AuthenticationController.java
+++ pizza/src/main/java/ch/fhnw/pizza/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package ch.fhnw.pizza.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import ch.fhnw.pizza.data.domain.User;
 
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
 
 
 @RestController
@@ -16,11 +19,13 @@ public class AuthenticationController {
     @PostMapping("/login") // changed from Getmapping 
     //public String getLogin(@RequestParam String username, @RequestParam String password) {
         //if (username.equals("admin") && password.equals("password")) {
-        public String getLogin(@RequestBody User user) {    
+        public RedirectView getLogin(@RequestBody User user) {    
         if (user.getUserName().equals("admin") && user.getPassword().equals("password")) {    
-            return "Login successful!";
+           // return "Login successful!";
+           return new RedirectView("/myprofile");
         } else {
-            return "Invalid credentials!";
+            return new RedirectView("/login?error=true"); 
+           // return "Invalid credentials!";
         }
     }
     
