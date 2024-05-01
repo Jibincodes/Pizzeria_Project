e07c26118c6e136f6ad27ff0e1e57eb753400cd1
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-01 00:43:59 +0000

    building the required needs of domain

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/domain/Payment.java
@@ -0,0 +1,64 @@
+package ch.fhnw.pizza.data.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;   
+    
+    //@Column(name = "finalprice")
+    //private Double finalprice;
+
+    //onetoone mapping has to be done  between order and payment
+    //it is unidirectional for sure 
+    //but ask where mapped by should be done
+    //-----------------------------------
+    @OneToOne(mappedBy = "payment")
+    //or is it using the join
+    private Order order;
+
+    public Double getFinalprice() {
+        return order.getFinalprice();
+    }
+    //-----------------------------------
+    @Column(name = "completed")
+    private Boolean completed;
+
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+   
+
+   // public Double getFinalprice() {
+   //     return finalprice;
+   // }
+
+   // public void setFinalprice(Double finalprice) {
+   //     this.finalprice = finalprice;
+    //}
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
