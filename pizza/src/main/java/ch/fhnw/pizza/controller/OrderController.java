340266651ba128af1f79b7a8a193c0c90949b056
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-01 18:27:09 +0000

    changes to order controller

--- pizza/src/main/java/ch/fhnw/pizza/controller/OrderController.java
+++ pizza/src/main/java/ch/fhnw/pizza/controller/OrderController.java
@@ -1,6 +1,8 @@
 
 package ch.fhnw.pizza.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,16 @@ import ch.fhnw.pizza.data.domain.Order;
 @RestController
 public class OrderController {
     
-    @PostMapping("/orders")
+    @PostMapping("/order")
     public String placeOrder(@RequestBody Order order) {
         // Logic to process the order
-        // ...// create order
+        //create order
+     //   List<Order> orders = // Logic to fetch all orders from the database or any other data source
+
+        // Convert the list of orders to a string representation
+    //    String ordersString = orders.toString();
+
+     //   return ordersString;
         
         return "Order placed successfully!";
     }
