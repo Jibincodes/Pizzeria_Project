9051c1bad5d02f270b07de9ee297aaa98659e2ea
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-18 23:49:21 +0000

    To do: Order service

---
+++ pizza/src/main/java/ch/fhnw/pizza/business/service/OrderService.java
@@ -0,0 +1,13 @@
+package ch.fhnw.pizza.business.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+   //TODO: Implement the OrderService 
+   //The OrderService should have methods to add, update, delete and get orders
+
+   //Finding order by id
+
+}
