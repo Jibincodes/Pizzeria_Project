0f6ab53e94e0f25ec6b69637530694846bd60920
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-19 02:11:38 +0000

    to discuss and setup for other end points in security config

--- pizza/src/main/java/ch/fhnw/pizza/security/SecurityConfig.java
+++ pizza/src/main/java/ch/fhnw/pizza/security/SecurityConfig.java
@@ -43,6 +43,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests( auth -> auth
                      //   .requestMatchers("/login").permitAll() // added by me today check with Devid
                         .requestMatchers("/menu").hasRole("USER") //note that the role need not be prefixed with "ROLE_"
+                    //    .requestMatchers("/order").hasRole("USER")
+                    //    .requestMatchers("/order").hasRole("ADMIN")
                         .requestMatchers("/menu/pizza/**").hasRole("ADMIN") //note that the role need not be prefixed with "ROLE_"
                         .requestMatchers("/menu/**",
                                                     "/**", //allow access to the home page
