9ebed08d173612841b21b4ff71faa5d0387de894
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-19 01:04:56 +0000

    required Jpa Repository

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ch.fhnw.pizza.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.fhnw.pizza.data.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+
+}
