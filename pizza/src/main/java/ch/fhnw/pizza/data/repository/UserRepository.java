cb2bd906a23a7de537fa0d4b7f38a4e3262ed94f
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-19 21:54:02 +0000

    no changes

--- pizza/src/main/java/ch/fhnw/pizza/data/repository/UserRepository.java
+++ pizza/src/main/java/ch/fhnw/pizza/data/repository/UserRepository.java
@@ -7,7 +7,5 @@ import ch.fhnw.pizza.data.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByUserName(String userName);
-    
-
+    User findByUserName(String userName);   
 }
