2a9822f3415d4f0a56b4896aec5fb379f5a689c4
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-19 02:49:48 +0000

    CRUD focused Order service

--- pizza/src/main/java/ch/fhnw/pizza/business/service/OrderService.java
+++ pizza/src/main/java/ch/fhnw/pizza/business/service/OrderService.java
@@ -1,13 +1,66 @@
 package ch.fhnw.pizza.business.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ch.fhnw.pizza.data.domain.Order;
+import ch.fhnw.pizza.data.domain.Pizza;
+import ch.fhnw.pizza.data.repository.OrderRepository;
 
 @Service
 public class OrderService {
 
-   //TODO: Implement the OrderService 
+@Autowired
+private OrderRepository orderRepository;
+
    //The OrderService should have methods to add, update, delete and get orders
 
    //Finding order by id
+   public Order findOrderById(Long id) {
+     try {
+         Order order = orderRepository.findById(id).get();
+         return order;
+     } catch (Exception e) {
+         throw new RuntimeException("Order with id " + id + " not found");
+     } 
+    }
+
+    //adding order
+    public Order addOrder(Order order) throws Exception
+    {
+        if(order.getFinalprice() != null) {
+            return orderRepository.save(order);
+        }
+        throw new Exception("Invalid order");
+    }
+
+    //updating order
+    public Order updateOrder(Long id, Order order) throws Exception {
+        Order orderToUpdate = orderRepository.findById(id).get();
+        if(orderToUpdate != null) {
+            if(order.getFinalprice() != null)
+                orderToUpdate.setFinalprice(order.getFinalprice());
+            return orderRepository.save(orderToUpdate);
+        }
+        throw new Exception("Order with id " + id + " does not exist");
+    }
+
+    //deleting order
+    public void deleteOrder(Long id) throws Exception {
+        if(orderRepository.existsById(id)) {
+            orderRepository.deleteById(id);
+        } else
+            throw new Exception("Order with id " + id + " does not exist");
+    }
+
+    //get all orders
+      public List<Order> getAllOrders() {
+       // List<Order> orderList = orderRepository.findAll();
+        //return orderList;
+        return orderRepository.findAll();
+      }
+      
 
 }
