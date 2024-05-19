1880c4439e9c935224c68538b12e73c0bae701ad
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-19 23:32:25 +0000

    no changes actually

--- pizza/src/main/java/ch/fhnw/pizza/data/repository/UserRepository.java
+++ pizza/src/main/java/ch/fhnw/pizza/data/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package ch.fhnw.pizza.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ch.fhnw.pizza.data.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByUserName(String userName);   
+   // @Query("SELECT u FROM User u WHERE u.userName = :userName")
+   // User findByUserName(@Param("userName") String userName);  
+   User findByUserName(String userName);
 }
