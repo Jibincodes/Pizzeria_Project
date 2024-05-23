c7bc6d0e56b26af36170036c152bd68dbdeb0bb1
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-23 08:52:48 +0000

    for security config to connect with users

---
+++ pizza/src/main/java/ch/fhnw/pizza/business/service/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package ch.fhnw.pizza.business.service;
+
+import java.util.Collection;
+import java.util.List;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import ch.fhnw.pizza.data.domain.User;
+
+public class UserDetailsImpl implements UserDetails {
+    private String userName;
+    private String password;
+    private List <GrantedAuthority> authorities;
+
+     public UserDetailsImpl(User user) {
+        userName=user.getUserName();
+        password=user.getPassword();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return userName;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+}
