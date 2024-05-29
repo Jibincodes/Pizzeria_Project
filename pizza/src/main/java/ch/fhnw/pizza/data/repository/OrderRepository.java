216b2e4b93a450449f5e5802fa72555d4fa0dab3
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-05-29 23:38:00 +0000

    required method

--- pizza/src/main/java/ch/fhnw/pizza/data/repository/OrderRepository.java
+++ pizza/src/main/java/ch/fhnw/pizza/data/repository/OrderRepository.java
@@ -1,13 +1,17 @@
 package ch.fhnw.pizza.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ch.fhnw.pizza.data.domain.Order;
+import ch.fhnw.pizza.data.domain.User;
 import jakarta.transaction.Transactional;
 
 @Repository
 @Transactional
 public interface OrderRepository extends JpaRepository<Order, Long>{
+    List<Order> findByUser(User user);
 
 }
